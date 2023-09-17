47e065faaeb61d0ea29b3356d5955915e61d3ce5
HenriqueNizolli <63325201+HenriqueNizolli@users.noreply.github.com>
2023-09-17 16:40:50 -0300

    test opencv dnn

--- src/main.py
+++ src/main.py
@@ -8,6 +8,11 @@ cam = cv2.VideoCapture(0)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+modelFile = "opencv_face_detector_uint8.pb"
+configFile = "opencv_face_detector.pbtxt"
+net = cv2.dnn.readNetFromTensorflow(modelFile, configFile)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
 while True:
     isRead, frame = cam.read()
@@ -15,10 +20,29 @@ while True:
     if not isRead:
         print("Error !!!")
         break
-
+        
     frame = cv2.flip(frame, 1)
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('assets', frame)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    frameHeight = frame.shape[0]
+    frameWidth = frame.shape[1]
+    blob = cv2.dnn.blobFromImage(gray_frame, 1.0, (300, 300), [104, 117, 123], False, False)
+
+    net.setInput(blob)
+    detections = net.forward()
+    bboxes = []
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > 0.7:
+            x1 = int(detections[0, 0, i, 3] * frameWidth)
+            y1 = int(detections[0, 0, i, 4] * frameHeight)
+            x2 = int(detections[0, 0, i, 5] * frameWidth)
+            y2 = int(detections[0, 0, i, 6] * frameHeight)
+            bboxes.append([x1, y1, x2, y2])
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight / 150)), 8, )
+
+    cv2.imshow('img', frame)
     cv2.waitKey(1)
 
 # Liberando a camera
