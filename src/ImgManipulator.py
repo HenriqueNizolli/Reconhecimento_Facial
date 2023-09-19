a6ce2537d61247c7baea419700ef09fdb77bfe69
HenriqueNizolli <63325201+HenriqueNizolli@users.noreply.github.com>
2023-09-18 23:59:09 -0300

    teste haars

--- src/ImgManipulator.py
+++ src/ImgManipulator.py
@@ -1,10 +1,9 @@
 import cv2
 
 
-def drawrectagle(frame, faces):
+def drawRectagle(frame, faces, color):
     for x, y, w, h in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
 
     return frame
-
 
--- src/main.py
+++ src/main.py
@@ -1,31 +1,28 @@
 # Imports
 import cv2
-import dlib
-
+import keyboard
 
 # Iniciando a camera
 cam = cv2.VideoCapture(0)
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-faceClassifier = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+faceClassifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt.xml')
 
 while True:
     isRead, frame = cam.read()
 
-    if not isRead:
-        print("Error !!!")
+    if (not isRead) or (keyboard.is_pressed('Esc')):
+        print("Shutdown.....")
         break
-        
+
     frame = cv2.flip(frame, 1)
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     faces = faceClassifier.detectMultiScale(gray_frame)
 
     for x, y, w, h in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     cv2.imshow('img', frame)
     cv2.waitKey(1)
