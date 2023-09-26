4333c8352df619743ebc9b18a9b2461e6da1a735
HenriqueNizolli <63325201+HenriqueNizolli@users.noreply.github.com>
2023-09-26 16:56:53 -0300

    face_teste

--- src/encoder.py
+++ src/encoder.py
@@ -1,2 +1,21 @@
+import os
 import cv2
 import face_recognition
+
+
+def encoding_imgs(img_list):
+    encodelist = []
+    for img in img_list:
+        encode = face_recognition.face_encodings(img[0])
+        encodelist.append(encode)
+    return encodelist
+
+
+def list_imgs(path):
+    imglist = []
+    for pessoa in os.listdir(path):
+        for img_nome in os.listdir(path + '/' + pessoa):
+            img = cv2.imread(path + '/' + pessoa + '/' + img_nome)
+            imglist.append([img, pessoa])
+    return imglist
+
--- src/main.py
+++ src/main.py
@@ -2,13 +2,20 @@
 import cv2
 import keyboard
 import face_recognition
+import numpy as np
+
+#my imports
+import encoder
+
+#database faces
+db_faces = encoder.list_imgs('../assets/img')
+db_enc = encoder.encoding_imgs(db_faces)
+print(db_faces)
 
 # Iniciando a camera
 cam = cv2.VideoCapture(0)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-
-faceClassifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt.xml')
 
 while True:
     isRead, frame = cam.read()
@@ -19,11 +26,21 @@ while True:
 
     frame = cv2.flip(frame, 1)
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_recognition.face_locations(gray_frame)
+    enc_faces = face_recognition.face_encodings(frame, faces)
 
-    faces = faceClassifier.detectMultiScale(gray_frame)
+    for enc, loc in zip(enc_faces, faces):
+        match = face_recognition.compare_faces(db_enc, enc)
+        dis = face_recognition.face_distance(db_enc, enc)
+        gg = np.argmin(dis)
 
-    for x, y, w, h in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        print(match)
+        print(dis)
+        print(gg)
+
+        if match[gg]:
+            for x, y, w, h in faces:
+                cv2.rectangle(frame, (y, x), (h, w), (0, 255, 0), 2)
 
     cv2.imshow('img', frame)
     cv2.waitKey(1)
